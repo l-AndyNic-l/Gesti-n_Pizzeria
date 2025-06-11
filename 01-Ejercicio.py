dd42c4b2022399b3b3a38e6fe0a06d8b177578fe
l-AndyNic-l <adri.lopez@duocuc.cl>
2025-06-10 18:40:55 -0600

    02: Registro de pizzas terminado

--- 01-Ejercicio.py
+++ 01-Ejercicio.py
@@ -1,7 +1,7 @@
 # Sistema de Gestión de Pedidos para una Pizzería.
 
 # Menu con las siguientes opciones: 
-# Registrar nuevos productos: Codigo, Nombre, Tipo de Masa, precio unitario, stock disponibles, INGREDIENTES
+# Registrar nuevos productos: precio unitario, stock disponibles, INGREDIENTES
 # Tomar pedidos: Descontar del  stock, calcular compra | nombre cliente, pizza comprada, cantidad y el total pagado
 # Calcular totales: calcular compra y ventas del dia.
 # Registro de ventas realizadas: mostrar que pizzas se vendieron, cuuantas y el total.
@@ -17,6 +17,42 @@ menu = """------------------------- M E N Ú -------------------------
 -----------------------------------------------------------
 """
 
+tipos_masas = """*** Tipos de masas ***
+1) Napolitana.
+2) Americana.
+3) Romana.
+4) Italiana.
+"""
+
+masas = ( "Napolitana", "Romana", "Italiana", "Americana" )
+
+pizzas = [
+    {
+        "codigo": 7811,
+        "nombre": "Pepperoni",
+        "ingredientes": [ "Salsa De Tomate", "Queso Parmesano", "Pepperoni" ],
+        "tipo_masa": "Napolitana",
+        "precio": 13900,
+        "stock": 20
+    },
+        {
+        "codigo": 7812,
+        "nombre": "Canadian Bacon",
+        "ingredientes": [ "Salsa De Tomate", "Queso Mozzarella", "Lomito Canadiense", "Tocino", "Queso Parmesano", "Queso Romano" ],
+        "tipo_masa": "Romana",
+        "precio": 15990,
+        "stock": 15
+    },
+        {
+        "codigo": 7813,
+        "nombre": "Margarita",
+        "ingredientes": [ "Salsa De Tomate", "Queso Mozzarella", "Albahaca", "Pimienta Negra", "Aceite Oliva" ],
+        "tipo_masa": "Italiana",
+        "precio": 19990,
+        "stock": 10
+    },
+]
+
 while True:
     os.system( "cls" )
     print( menu )
@@ -24,7 +60,30 @@ while True:
     opcion = input( "Ingrese una opción: " )
 
     if opcion == "1":
-        pass
+        os.system( "cls" )
+        print( "----- Registrar Nueva Pizza -----" )
+
+        codigo = int( input( "Ingrese el codigo: " ) )
+        nombre = input( "Ingrese el nombre: " )
+
+        print( tipos_masas )
+        tipo_masa = int( input( "Ingrese el número de la masa: " ) )
+        masa = masas[ tipo_masa - 1 ]
+
+        precio = int( input( "Ingrese el precio unitario: " ) )
+        stock = int( input( "Ingrese el stock disponible" ) )
+
+        pizza = {
+            "codigo": codigo,
+            "nombre": nombre,
+            "tipo_masa": masa,
+            "precio": precio,
+            "stock": stock
+        }
+
+        pizzas.append( pizza )
+
+        print( "Pizza registrada exitosamente!" )
 
     elif opcion == "2":
         pass
